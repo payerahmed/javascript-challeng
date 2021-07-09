883be55c24b6d389d457337b76f4700f04b067b5
payerahmed <mdpayer125@gmail.com>
2021-07-09 16:02:12 +0600

    math sequences

--- App.js
+++ App.js
@@ -11,3 +11,27 @@ function longestWords(str) {
 }
 console.log(longestWords(' i woke up early today'));
 console.log(longestWords(' i went straigh to the beach'));
+
+function mathSeeqences(arr) {
+  let arith = new Set();
+  let geo = new Set();
+
+  for (let i = 1; i < arr.length; i++) {
+    let number1 = arr[i] - arr[i - 1];
+    arith.add(number1);
+    let number2 = arr[i] / arr[i - 1];
+    geo.add(number2);
+  }
+  if (arith.size === 1) {
+    return 'Arithmetic';
+  }
+  if (geo.size === 1) {
+    return 'Geometric';
+  }
+
+  return -1;
+}
+
+console.log(mathSeeqences([10, 20, 30, 40]));
+console.log(mathSeeqences([2, 4, 8, 16 ]));
+console.log(mathSeeqences([2, 5, 67, 14, 68, 89]));
