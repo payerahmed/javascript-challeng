e32854db16c9dc7b1ceb07fe24f697ff6b9cfa05
payerahmed <mdpayer125@gmail.com>
2021-07-10 18:31:02 +0600

    unique value with set

--- App.js
+++ App.js
@@ -52,12 +52,23 @@ console.log(capitalizeWords('i woke up early today'));
 console.log(capitalizeWords('i went straigh to the beach'));
 
 function unique(str) {
-  for (let i = 0; i < str.length; i++) {
-    if (str.lastIndexOf(str[i]) !== i) {
-      return false;
-    }
-  }
-  return true;
+  // for (let i = 0; i < str.length; i++) {
+  //   if (str.lastIndexOf(str[i]) !== i) {
+  //     return false;
+  //   }
+
+  return new Set(str).size === str.length;
+
+  // let tempStr = new Set()
+  // for(let letter of str){
+  //   if(tempStr.has(letter)){
+  //     return false
+  //   }
+  //   tempStr.add(letter)
+  // }
+
+  //   }
+  //   return true;
 
   // let value = {};
   // for (let letter of str) {
